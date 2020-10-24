2666e555bf1714e25cd49bfa0831cfd848502a40
Robert Dates <rsndates@yahoo.com>
2020-10-23 20:14:11 -0400

    Initial Framework commit with Feed model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Robert Dates on 10/23/20.
+//
+
+import Foundation
+
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
