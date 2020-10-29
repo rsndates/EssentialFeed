763e7dd99fb9736f98ab64892839ced93653a3ab
Robert Dates <rsndates@yahoo.com>
2020-10-28 23:03:06 -0400

    Delivers empty items array on 200 HTTP response with JSON empty list

--- EssentialFeedTests/RemoteFeederLoaderTests.swift
+++ EssentialFeedTests/RemoteFeederLoaderTests.swift
@@ -62,6 +62,17 @@ class RemoteFeederLoaderTests: XCTestCase {
         }
     }
     
+    func test_load_deliversNoItemsOn200HTTPReponseWithEmptyJSONList() {
+        let (sut, client) = makeSUT()
+        var captureResults = [RemoteFeedLoader.Result]()
+        sut.load { captureResults.append($0) }
+        
+        let emptyListJSON = Data("{\"items\": []}".utf8)
+        client.complete(withStatusCode: 200, data: emptyListJSON)
+        
+        XCTAssertEqual(captureResults, [.success([])])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
