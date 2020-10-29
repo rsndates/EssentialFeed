cf8b3947d3f859e518cc827b1a8a71bd073c29ad
Robert Dates <rsndates@yahoo.com>
2020-10-29 01:57:35 -0400

    Replace magic number with variable to give extra context

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -68,9 +68,11 @@ private class FeedItemsMapper {
             return FeedItem(id: id, description: description, location: location, imageURL: image)
         }
     }
+    
+    static var OK_200: Int { return 200 }
 
     static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
-        guard response.statusCode == 200 else { throw RemoteFeedLoader.Error.invalidData }
+        guard response.statusCode == OK_200 else { throw RemoteFeedLoader.Error.invalidData }
         let root = try JSONDecoder().decode(Root.self, from: data)
         return root.items.map { $0.item }
     }
